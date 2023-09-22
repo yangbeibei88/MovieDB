6dc581a306db65fb830eab50ad59a28c87e60c07
BeibeiYang <chrisyangber@hotmail.com>
2023-09-23 01:40:39 +1000

    display popular movies on home page

---
+++ js/script.js
@@ -0,0 +1,98 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+// Hilight active links
+function highlightActive() {
+  const navLinks = document.querySelectorAll(".nav-link");
+  console.log(navLinks);
+  // method 1:
+  // switch (global.currentPage) {
+  //   case "/":
+  //   case "/index.html":
+  //     navLinks[0].classList.add("active");
+  //     break;
+  //   case "/shows.html":
+  //     navLinks[1].classList.add("active");
+  //     break;
+  // }
+  // method 2:
+  navLinks.forEach((navLink) => {
+    // another way to write if statement:
+    // if(navLink["pathname"] === global.currentPage)
+    if (navLink.getAttribute("href") === global.currentPage) {
+      navLink.classList.add("active");
+    }
+    console.log(navLink["pathname"]);
+    console.log(global.currentPage);
+    console.log(navLink);
+  });
+}
+
+// display popular movies on home page
+function displayPopularMovies() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMWUyN2Y1MjQ2NWM4MWY1YjNlMWQ4OGM1NjE1MTUzMyIsInN1YiI6IjY0YmI1NDc0ZWI3OWMyMDBlMjhjYjYwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PiSeqGdciSnRUE7KdUT-kkcLVLYk2nBc_SZdbxP9TTo",
+    },
+  };
+
+  fetch(
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.results);
+      const movies = data.results;
+      movies.forEach((movie) => {
+        const popularMovies = document.getElementById("popular-movies");
+        const div = document.createElement("div");
+        div.className = "card";
+        div.innerHTML = `<a href="movie-details.html?id=${movie.id}">
+
+        ${
+          movie.poster_path
+            ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />`
+            : `<img src="..images/no-image.jpg" alt="${movie.title}" />`
+        }
+
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release:
+          ${movie.release_date}</small>
+        </p>
+      </div>`;
+        popularMovies.appendChild(div);
+      });
+    })
+    .catch((err) => console.error(err));
+}
+
+function init() {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      console.log("home");
+      displayPopularMovies();
+      break;
+    case "/shows.html":
+      console.log("shows");
+      break;
+    case "/movie-details.html":
+      console.log("movie details");
+    case "/tv-details.html":
+      console.log("tv details");
+    case "/search.html":
+      console.log("search");
+  }
+  highlightActive();
+}
+
+document.addEventListener("DOMContentLoaded", init);
